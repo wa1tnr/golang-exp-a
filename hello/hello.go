d7b818587db609fcd31f72ef25e5070b59874dd7
wa1tnr <wa1tnr@users.noreply.github.com>
2024-09-30 18:44:04 +0000

    initial go program from tutorial

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+// Mon 30 Sep 17:41:17 UTC 2024
+// https://go.dev/doc/tutorial/getting-started
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("stand-in for a hello world type utterance")
+}
+
+// end.
