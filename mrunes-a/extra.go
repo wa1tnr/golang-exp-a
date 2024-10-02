ba75cd823fb5291ab7f0676111f5ad7a8e2ec0ab
wa1tnr <wa1tnr@users.noreply.github.com>
2024-10-02 12:39:43 +0000

    Update three files: mkfile runes, extra .go files
    Makefile:
      add extra.go to build target
      add tidy target  go mod tiny
      add extra.go to run target
    
    extra.go:
      make unique from templated earlier file
      make new function _this_ and make it unique
    
    runes.go:
      call function _this_ to test multi-file compile and run
    
    EXPERIMENT: use some punctuation in the commit message
      includes use of: colon, comma, decimal pt and underscore
      did NOT try parens L or R
    	modified:   Makefile
    	modified:   extra.go
    	modified:   runes.go
    
    On branch dvlp-aa

--- mrunes-a/extra.go
+++ mrunes-a/extra.go
@@ -1,16 +1,17 @@
-/* runes.go */
-/* Tue  1 Oct 13:14:22 UTC 2024 */
-/* src: google 'go rune literal' with uncredited 'AI Overview' */
+/* extra.go */
+/* Wed  2 Oct 12:19:21 UTC 2024 */
+/* was: */
+/*   Tue  1 Oct 13:14:22 UTC 2024 */
 
 package main
 
 import "fmt"
 
-func main() {
-	var r1 rune = 'a'
+func this() {
+	var r1 rune = 'b'
 	var r2 rune = 'π'
 	var r3 rune = '\n'
-	var r4 rune = '\u0041'
+	var r4 rune = '\u0046'
 
 	fmt.Printf("%c%c%c%c\n", r1, r2, r3, r4)
 }
