03fd47d85ded74cca044f308ae1004622086c989
wa1tnr <wa1tnr@users.noreply.github.com>
2024-10-02 13:27:22 +0000

    fancy print mod

--- mrunes-b/runes.go
+++ mrunes-b/runes.go
@@ -1,8 +1,12 @@
 /* runes.go */
-/* Wed  2 Oct 12:19:21 UTC 2024 */
+/* Wed  2 Oct 13:20:35 UTC 2024 */
 /* was: */
+/*   Wed  2 Oct 12:19:21 UTC 2024 */
 /*   Tue  1 Oct 13:14:22 UTC 2024 */
 /* src: google 'go rune literal' with uncredited 'AI Overview' */
+
+/* golang/mrunes-b */
+/* variant: fancy printing */
 
 package main
 
@@ -11,11 +15,15 @@ import "fmt"
 func main() {
 	var r1 rune = 'a'
 	var r2 rune = 'π'
-	var r3 rune = '\n'
-	var r4 rune = '\u0041'
+	var r3 rune = '\u0010' /* h 8 i 9 j 10 */
+	var r4 rune = '\u0008' /* h 8 */
+	var r5 rune = '\r'
+	var r6 rune = '\n'
+	var r7 rune = '\u0041'
 
-	fmt.Printf("%c%c%c%c\n", r1, r2, r3, r4)
-        this()
+	fmt.Printf("\n  runes.go:\n    %c\n    %c\n%c%c       (newline)%c%c    %c\n", r1, r2, r3, r4, r5, r6, r7)
+	fmt.Println("       .. and now, call this() in sequence:\n")
+	this()
 }
 
 /* end. */
