e51747900c2dbbebe60623c2ad2041fbcd0b965b
wa1tnr <wa1tnr@users.noreply.github.com>
2024-10-03 12:03:16 +0000

    interim edit extra notes

--- mrunes-b/runes.go
+++ mrunes-b/runes.go
@@ -12,16 +12,33 @@ package main
 
 import "fmt"
 
+/* one flaw one note:
+ * ^M visible at end of '(newline)' utterance as in '(newline)^M'
+ * the dot in 'runes.go' (printed) aligns with the ( of  '(newline)'
+ */
+
+/* 0x0d 0x0a  cr lf */
+/* 13   10 */
+
 func main() {
 	var r1 rune = 'a'
 	var r2 rune = 'π'
-	var r3 rune = '\r'
-	var r4 rune = '\n'
-	var r5 rune = '\u0041'
+	var r3 rune = '\n'
+	var r4 rune = '\u0041'
+
+	fmt.Printf("\n  runes.go:\n    ",)
 
 	fmt.Printf(
-		"\n  runes.go:\n    %c\n    %c\n       (newline)%c%c    %c\n",
-		r1, r2, r3, r4, r5,
+                                      "%c\n    ", r1,)
+	fmt.Printf(
+                                              "%c\n       ", r2,)
+	fmt.Printf(
+                                                         "(newline)%c    ", r3,)
+	fmt.Printf(
+                                                                        "%c\n",r4,)
+//	fmt.Printf("\n  runes.go:\n    %c\n    %c\n       (newline)%c    %c\n",
+	fmt.Printf("\n  runes.go:\n    %c\n    %c\n       (newline)%c    %c\n",
+		r1, r2, r3, r4,
 	)
 
 	fmt.Println("       .. and now, call this() in sequence:\n")
